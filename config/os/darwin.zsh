82d27762216e90655c191fe265680ec6e6a55e2d
Zachary Elliott <contact@zell.io>
2024-08-16 10:31:32 -0400

    Cleans up XDG run dir on osx

--- config/os/darwin.zsh
+++ config/os/darwin.zsh
@@ -5,3 +5,10 @@
 path_dirs_system=(
 	"${(ps/:/)"$(env - /usr/libexec/path_helper -c | cut -d'"' -f2)"}"
 )
+
+export XDG_RUNTIME_DIR="$XDG_CACHE_HOME"/run
+
+if [ ! -d "$XDG_RUNTIME_DIR" ]; then
+	mkdir "$XDG_RUNTIME_DIR"
+	chmod 0700 "$XDG_RUNTIME_DIR"
+fi
